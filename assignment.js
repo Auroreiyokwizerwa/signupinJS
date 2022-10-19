55c164b022a38574f96e8b4ee398b2601aab787a
Aurore <hirwa.gael@gmail.com>
2022-10-18 23:47:26 -0700

    new

---
+++ assignment.js
@@ -0,0 +1,12 @@
+
+function log(){
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var output= document.getElementById("output");
+    var out= document.getElementById("out");
+    // alert("button clicked");
+    output.innerHTML="please enter your name";
+    out.innerHTML="please enter your email " ;
+    document.getElementById("output").style.color="red";
+    document.getElementById("out").style.color="red";
+}
